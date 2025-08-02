2d5014e7ae463dad7ede49bed9db5daee65afe31
edend85 <117260021+edend85@users.noreply.github.com>
2025-08-02 12:22:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="chengriner-site">
+  <BrowserRouter basename="/chengriner-site">
   <App />
 </BrowserRouter>
 );
