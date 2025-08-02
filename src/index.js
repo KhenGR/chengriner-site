23e886eb483d4af449f2dda2d5af1383eaf34f19
edend85 <117260021+edend85@users.noreply.github.com>
2025-08-02 12:45:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-import { HashRouter as Router } from "react-router-dom";root.render();
-
-
 root.render(
-  <Router>
+  <HashRouter>
   <App />
-</Router>
+</HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
